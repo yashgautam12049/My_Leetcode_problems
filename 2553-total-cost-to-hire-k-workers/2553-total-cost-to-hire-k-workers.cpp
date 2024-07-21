a6d0a81d6d758ab21c4decac7fccb4aaa59a30b4
yashgautam12049 <97456693+yashgautam12049@users.noreply.github.com>
2024-07-21 17:31:43 +0530

    Time: 128 ms (47.51%), Space: 77 MB (46.52%) - LeetHub

---
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.cpp
@@ -0,0 +1,39 @@
+using ll=long long;
+class Solution {
+public:
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        priority_queue<int,vector<int>,greater<int>>pq1;
+        priority_queue<int,vector<int>,greater<int>>pq2;
+        long long n=costs.size();
+        long long i=0;
+        long long j=n-1;
+        long long ans=0;
+        while(k--){
+            long long k1=candidates;
+            while(k1-- && pq1.size()!=candidates && i<=j){
+                pq1.push(costs[i]);
+                i++;
+            }
+            long long m=candidates;
+            while(m-- && pq2.size()!=candidates && i<=j){
+                pq2.push(costs[j]);
+                j--;
+            }
+            long long min_pq1=pq1.size()>0?pq1.top():INT_MAX;
+            long long min_pq2=pq2.size()>0?pq2.top():INT_MAX;
+            if(min_pq2<min_pq1){
+                ans+=pq2.top();
+                pq2.pop();
+            }
+            else{
+                ans+=pq1.top();
+                pq1.pop();
+            }
+            // ans+=pq2.top()<pq1.top()?pq2.top():pq1.top();
+        }
+        // if(k!=0){
+           
+        // }
+        return ans;
+    }
+};
