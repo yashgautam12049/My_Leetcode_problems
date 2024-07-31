f355a59a423c4e291ec695023f26c5ddc4dfe374
yashgautam12049 <97456693+yashgautam12049@users.noreply.github.com>
2024-07-31 17:00:39 +0530

    Time: 99 ms (46.48%), Space: 24.7 MB (51.49%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
+    // int dp[100001][10000];
+    // int solve(int ind,string &s,int prev,vector<vector<int>>&dp){
+    //     //take
+    //     if(ind==s.size()) return 0;
+    //     if(dp[ind][prev+1]!=-1){
+    //         return dp[ind][prev+1];
+    //     }
+    //     int take=0;
+    //     if(prev==-1|| (s[ind]=='a' && s[prev]!='b') ||s[ind]=='b'){
+    //     take=1+solve(ind+1,s,ind,dp);
+    //     }
+    //     //not_take
+    //     int not_take=solve(ind+1,s,prev,dp);
+    //     return dp[ind][prev+1]=max(take,not_take);
+    // }
     int minimumDeletions(string s) {
-        int n = s.size();
-        int f[n + 1];
-        memset(f, 0, sizeof(f));
-        int b = 0;
-        for (int i = 1; i <= n; ++i) {
-            if (s[i - 1] == 'b') {
-                f[i] = f[i - 1];
-                ++b;
-            } else {
-                f[i] = min(f[i - 1] + 1, b);
+        int n=s.size();
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        // memset(dp,-1,sizeof(dp));
+        stack<char>st;
+        int cnt=0;
+        for(auto it:s){
+            if(!st.empty() && st.top()=='b' && it=='a'){
+                st.pop();
+                cnt++;
+            }
+            else{
+                st.push(it);
             }
         }
-        return f[n];
+        return cnt;
     }
 };
