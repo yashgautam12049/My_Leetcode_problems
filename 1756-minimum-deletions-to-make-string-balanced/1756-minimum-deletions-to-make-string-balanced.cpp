ff6cd3d71ddc73777c7f44b308a0692220300d48
yashgautam12049 <97456693+yashgautam12049@users.noreply.github.com>
2024-07-31 17:37:23 +0530

    Time: 98 ms (47.73%), Space: 53.2 MB (14.4%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-    // int dp[100001][10000];
-    // int solve(int ind,string &s,int prev,vector<vector<int>>&dp){
-    //     //take
-    //     if(ind==s.size()) return 0;
-    //     if(dp[ind][prev+1]!=-1){
-    //         return dp[ind][prev+1];
-    //     }
-    //     int take=0;
-    //     if(prev==-1|| (s[ind]=='a' && s[prev]!='b') ||s[ind]=='b'){
-    //     take=1+solve(ind+1,s,ind,dp);
-    //     }
-    //     //not_take
-    //     int not_take=solve(ind+1,s,prev,dp);
-    //     return dp[ind][prev+1]=max(take,not_take);
-    // }
     int minimumDeletions(string s) {
-        int n=s.size();
-        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        // memset(dp,-1,sizeof(dp));
-        stack<char>st;
-        int cnt=0;
-        for(auto it:s){
-            if(!st.empty() && st.top()=='b' && it=='a'){
-                st.pop();
-                cnt++;
-            }
-            else{
-                st.push(it);
+        int n = s.size();
+        int countb = 0;
+        int counta = 0;
+        vector<int> count_b(n, 0);
+        vector<int> count_a(n, 0);
+        for (int i = 0; i < n; i++) {
+            count_b[i] = countb;
+            if (s[i] == 'b') {
+                countb++;
             }
         }
-        return cnt;
+        for (int i = n - 1; i >= 0; i--) {
+            count_a[i] = counta;
+            if (s[i] == 'a') {
+                counta++;
+            }
+        }
+        int ans = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            ans = min(ans, count_a[i] + count_b[i]);
+        }
+        return ans;
     }
 };
