1bf001a3614f1bb12551ef9a9adcbaed79151b3a
yashgautam12049 <97456693+yashgautam12049@users.noreply.github.com>
2024-08-20 12:07:57 +0530

    Time: 15 ms (36.83%), Space: 7.4 MB (77.36%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
+int solve(int copytopaste,int paste,int n){
+    if(copytopaste==n){
+        return 0;
+    }
+    if(copytopaste>n){
+        return 100000;
+    }
+    int a=2+solve(copytopaste+copytopaste,copytopaste,n);
+    int b=1+solve(copytopaste+paste,paste,n);
+    return min(a,b);
+}
     int minSteps(int n) {
-        if (n == 1) return 0;
-        
-        int steps = 0;
-        int factor = 2;
-        
-        while (n > 1) {
-            while (n % factor == 0) {
-                steps += factor;
-                n /= factor;
-            }
-            factor++;
+        if(n==1){
+            return 0;
         }
-        
-        return steps;
+        return 1+solve(1,1,n);
     }
 };
