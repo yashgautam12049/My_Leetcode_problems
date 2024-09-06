2555034e04580ebca5e40422acc6424f4bf5f0d2
yashgautam12049 <97456693+yashgautam12049@users.noreply.github.com>
2024-09-07 02:07:22 +0530

    Time: 63 ms (48.29%), Space: 18 MB (90.3%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -1,75 +1,61 @@
-class dsu{
+class DSU{
     public:
-    vector<int>rank,parent,size;
-    dsu(int n){
-        rank.resize(n+1,0);
-        parent.resize(n+1,0);
-        size.resize(n+1);
-        for(int i=0;i<=n;i++){
+    vector<int>parent;
+    vector<int>rank;
+    DSU(int n){
+        parent.resize(n);
+        rank.resize(n,0);
+        for(int i=0;i<n;i++){
             parent[i]=i;
-            size[i]=1;
         }
     }
-    int findUpar(int node){
-        if(node==parent[node]){
-            return node;
+    int find_par(int u){
+        if(u==parent[u]){
+            return u;
         }
-        return parent[node]=findUpar(parent[node]);
+        return find_par(parent[u]);
     }
-    void unionbyrank(int u,int v){
-        int ulp_u=findUpar(u);
-        int ulp_v=findUpar(v);
-        if (rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v;
+    void union_by_rank(int u,int v){
+        int u_par=find_par(u);
+        int v_par=find_par(v);
+        if(u_par==v_par){
+            return;
         }
-        else if (rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u;
+        if(rank[u_par]>rank[v_par]){
+            parent[v_par]=u_par;
         }
-        else {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
+        else if(rank[v_par]>rank[u_par]){
+            parent[u_par]=v_par;
         }
-    }
-    void unionbysize(int u, int v) {
-        int ulp_u = findUpar(u);
-        int ulp_v = findUpar(v);
-        if (ulp_u == ulp_v) return;
-        if (size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
+        else{
+            parent[v_par]=u_par;
         }
     }
 };
 class Solution {
 public:
     int removeStones(vector<vector<int>>& stones) {
-        int r=0;
-        int c=0;
-        for(auto it:stones){
-            r=max(r,it[0]);
-            c=max(c,it[1]);
-        }
-        // int r=stones.size();
-        // int c=stones[0].size();
-        dsu ds(r+c+1);
-        map<int,int>mp;
-        for(auto it:stones){
-            int row=it[0];
-            int col=r+it[1]+1;
-            ds.unionbyrank(row,col);
-            mp[row]=1;
-            mp[col]=1;
-        }
-        int cnt=0;
-        for(auto it:mp){
-            if(ds.findUpar(it.first)==it.first){
-                cnt++;
+        int row=stones.size();
+        DSU ds(row);
+        for(int i=0;i<row;i++){
+            for(int j=i+1;j<row;j++){
+                int r=stones[i][0];
+                int c=stones[i][1];
+                int r_j=stones[j][0];
+                int c_j=stones[j][1];
+                if(r==r_j || c==c_j){
+                    if(ds.find_par(i)!=ds.find_par(j)){
+                    ds.union_by_rank(i,j);
+                    }
+                }
             }
         }
-        return stones.size()-cnt;
+        int group=0;
+        for(int i=0;i<row;i++){
+            if(ds.find_par(i)==i){
+                group++;
+            }
+        }
+        return row-group;
     }
 };
