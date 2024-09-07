6062634855e894eca20d0549f408d0ee7dd5f00d
yashgautam12049 <97456693+yashgautam12049@users.noreply.github.com>
2024-09-07 14:38:44 +0530

    Time: 9 ms (16.09%), Space: 11.4 MB (92.48%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    int dp[201][201];
-    int solve(int r,int c,vector<vector<int>>& dungeon){
+int dp[201][201];
+    int solve(int i,int j,vector<vector<int>>& dungeon){
+        //up
         int n=dungeon.size();
         int m=dungeon[0].size();
-        if(r<0 || r>=n || c<0 || c>=m) return INT_MAX;
-        
-        if(dp[r][c]!=-1){
-            return dp[r][c];
+        if(i<0 || i>=n || j<0 || j>=m){
+            return INT_MAX;
         }
-        int a=min(solve(r,c+1,dungeon),solve(r+1,c,dungeon));
-        if(a==INT_MAX){
-            a=1;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-        int res=0;
-        if(a-dungeon[r][c]>0){
-            res=a-dungeon[r][c];
+        int health= min(solve(i+1,j,dungeon),solve(i,j+1,dungeon));
+        //left;
+        if(health==INT_MAX){
+            health=1;
+        }
+        if((health-dungeon[i][j])>0){
+            health=health-dungeon[i][j];
         }
         else{
-            res=1;
+            health=1;
         }
- 
-        return dp[r][c]=res;
+        return dp[i][j]=health;
     }
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        int n=dungeon.size();
-        int m=dungeon[0].size();
         memset(dp,-1,sizeof(dp));
         return solve(0,0,dungeon);
     }
