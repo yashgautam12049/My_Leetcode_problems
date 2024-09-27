c64a76c0622f39ef8add8e6ec49d810dd080aa61
yashgautam12049 <97456693+yashgautam12049@users.noreply.github.com>
2024-09-27 22:20:05 +0530

    Time: 171 ms (37.4%), Space: 130.7 MB (15.5%) - LeetHub

--- 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
@@ -11,40 +11,47 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
-        vector<vector<int>> matrix(m, vector<int>(n, -1));
-        
-        int topRow = 0, bottomRow = m - 1, leftColumn = 0, rightColumn = n - 1;
-        
-        while (head) {
-            // Fill top row
-            for (int col = leftColumn; col <= rightColumn && head; ++col) {
-                matrix[topRow][col] = head->val;
-                head = head->next;
+        vector<vector<int>>grid(m,vector<int>(n,-1));
+        int top=0;
+        int bottom=m-1;
+        int front=0;
+        int back=n-1;
+        int dir=0;
+        while(head!=NULL){
+            if(dir==0){
+            for(int i=front;i<=back && head!=NULL;i++){
+            grid[top][i]=head->val;//right
+            head=head->next;
             }
-            topRow++;
+            top++;
+            }
+            else if(dir==1){
+            for(int i=top;i<=bottom && head!=NULL;i++){
+            grid[i][back]=head->val; //down
+            head=head->next;
+            }
+            back--;
+            }
+            else if(dir==2){
+            for(int i=back;i>=front && head!=NULL;i--){
+            grid[bottom][i]=head->val; //left
+            head=head->next;
+            }
+            bottom--;
+            }
+            else if(dir==3){
+            for(int i=bottom;i>=top && head!=NULL;i--){
+            grid[i][front]=head->val; //up
+            head=head->next;
+            }
+            front++;
+            }
+            dir++;
+            if(dir==4){
+                dir=0;
+            }
             
-            // Fill right column
-            for (int row = topRow; row <= bottomRow && head; ++row) {
-                matrix[row][rightColumn] = head->val;
-                head = head->next;
-            }
-            rightColumn--;
-            
-            // Fill bottom row
-            for (int col = rightColumn; col >= leftColumn && head; --col) {
-                matrix[bottomRow][col] = head->val;
-                head = head->next;
-            }
-            bottomRow--;
-            
-            // Fill left column
-            for (int row = bottomRow; row >= topRow && head; --row) {
-                matrix[row][leftColumn] = head->val;
-                head = head->next;
-            }
-            leftColumn++;
         }
-        
-        return matrix;
+        return grid;
     }
 };
