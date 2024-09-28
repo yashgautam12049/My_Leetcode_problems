7c4b1a12f1faccabad24bda971b80ca8f9681620
yashgautam12049 <97456693+yashgautam12049@users.noreply.github.com>
2024-09-28 21:01:21 +0530

    Time: 119 ms (62.4%), Space: 68.5 MB (90.07%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-int dp[51];
-    int solve(int i,string s,set<string>st){
-        int n=s.size();
-        //base case
-        if(i==n){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        //not take as extra 
-        int take=INT_MAX;
-        string a="";
-        for(int j=i;j<n;j++){
-            a+=s[j];
-            if(st.find(a)!=st.end()){
-                take=min(take,solve(j+1,s,st));
+    int dp[51];
+    
+    int minExtraChar(string s, vector<string>& dictionary) {
+        int n = s.size();
+        memset(dp, -1, sizeof(dp));
+        dp[n]=0;
+        set<string> st(dictionary.begin(), dictionary.end());
+        for (int i = n-1; i >= 0; i--) {
+             dp[i] = 1 + dp[i+1];
+            string a = "";
+            for (int j = i; j < n; j++) {
+                a += s[j];
+                if (st.find(a) != st.end()) {
+                    dp[i] = min(dp[i], dp[j+1]);
+                }
             }
         }
-        
-        //take as extra
-        int take_extra=1+solve(i+1,s,st);
-        return dp[i]= min(take,take_extra);
-    }
-    int minExtraChar(string s, vector<string>& dictionary) {
-        memset(dp,-1,sizeof(dp));
-        set<string>st(dictionary.begin(),dictionary.end());
-        return solve(0,s,st);
+        return dp[0];
     }
 };
