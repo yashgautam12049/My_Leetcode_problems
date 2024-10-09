8652448c9ca4a46a20bcbd58f36abeb2677fc60b
yashgautam12049 <97456693+yashgautam12049@users.noreply.github.com>
2024-10-09 13:15:14 +0530

    Time: 67 ms (26.36%), Space: 61.6 MB (74.55%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int l = 0;
-        int n = height.size();
-        int r = n - 1;
-        int maxarea = 0;
-        int area = 0;
-        int maxlen = 0;
-        int breadth = 0;
-
-        while (l < r) {
-            // Calculate the minimum height between the two pointers
-            maxlen = min(height[l], height[r]);
-            
-            // Calculate the breadth (distance between the two pointers)
-            breadth = r - l;
-
-            // Calculate the area
-            area = maxlen * breadth;
-
-            // Update the maximum area found so far
-            maxarea = max(maxarea, area);
-
-            // Move the pointer pointing to the smaller height
-            if (height[l] < height[r]) {
-                l++; // Move left pointer to the right
-            } else {
-                r--; // Move right pointer to the left
+        int i=0;
+        int j=height.size()-1;
+        int ans=0;
+        while(i<j){
+            int h=min(height[i],height[j]);
+            if(h==height[i]){
+                ans=max(ans,h*(j-i));
+                i++;
+            }else{
+                ans=max(ans,h*(j-i));
+                j--;
             }
         }
-
-        return maxarea;
+        return ans;
     }
 };
