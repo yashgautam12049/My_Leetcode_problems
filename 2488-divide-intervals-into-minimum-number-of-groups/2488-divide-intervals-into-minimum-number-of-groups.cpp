c65c8fc969a8893693aa0555a388f267bb381876
yashgautam12049 <97456693+yashgautam12049@users.noreply.github.com>
2024-10-12 14:43:59 +0530

    Time: 293 ms (55.86%), Space: 104.8 MB (61.71%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,39 +1,39 @@
-class DSU{
-    public:
-    vector<int>parent;
-    vector<int>rank;
-    DSU(int n){
-        parent.resize(n);
-        rank.resize(n);
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-        }
-    }
-    int find_par(int u){
-        if(parent[u]==u){
-            return u;
-        }
-        return find_par(parent[u]);
-    }
-    void union_by_rank(int u,int v){
-        int u_par=find_par(u);
-        int v_par=find_par(v);
-        if(u_par==v_par){
-            return;
-        }
-        if(rank[u_par]>rank[v_par]){
-            parent[v]=u;
-        }
-        else if(rank[v_par]>rank[u_par]){
-            parent[u]=v;
-        }
-        else{
-            parent[v]=u;
-            rank[u]++;
-        }
+// class DSU{
+//     public:
+//     vector<int>parent;
+//     vector<int>rank;
+//     DSU(int n){
+//         parent.resize(n);
+//         rank.resize(n);
+//         for(int i=0;i<n;i++){
+//             parent[i]=i;
+//         }
+//     }
+//     int find_par(int u){
+//         if(parent[u]==u){
+//             return u;
+//         }
+//         return find_par(parent[u]);
+//     }
+//     void union_by_rank(int u,int v){
+//         int u_par=find_par(u);
+//         int v_par=find_par(v);
+//         if(u_par==v_par){
+//             return;
+//         }
+//         if(rank[u_par]>rank[v_par]){
+//             parent[v]=u;
+//         }
+//         else if(rank[v_par]>rank[u_par]){
+//             parent[u]=v;
+//         }
+//         else{
+//             parent[v]=u;
+//             rank[u]++;
+//         }
 
-    }
-};
+//     }
+// };
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
