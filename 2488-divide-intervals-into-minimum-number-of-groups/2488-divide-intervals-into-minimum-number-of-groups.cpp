100c91eb3d63ac66cf12daf3ee3e652e81526325
yashgautam12049 <97456693+yashgautam12049@users.noreply.github.com>
2024-10-12 14:43:50 +0530

    Time: 311 ms (51.8%), Space: 104.7 MB (62.84%) - LeetHub

---
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
@@ -0,0 +1,53 @@
+class DSU{
+    public:
+    vector<int>parent;
+    vector<int>rank;
+    DSU(int n){
+        parent.resize(n);
+        rank.resize(n);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+    }
+    int find_par(int u){
+        if(parent[u]==u){
+            return u;
+        }
+        return find_par(parent[u]);
+    }
+    void union_by_rank(int u,int v){
+        int u_par=find_par(u);
+        int v_par=find_par(v);
+        if(u_par==v_par){
+            return;
+        }
+        if(rank[u_par]>rank[v_par]){
+            parent[v]=u;
+        }
+        else if(rank[v_par]>rank[u_par]){
+            parent[u]=v;
+        }
+        else{
+            parent[v]=u;
+            rank[u]++;
+        }
+
+    }
+};
+class Solution {
+public:
+    int minGroups(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        int n=intervals.size();
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(auto it:intervals){
+            int start=it[0];
+            int end=it[1];
+            if(!pq.empty() && start>pq.top()){
+                pq.pop();
+            }
+            pq.push(end);
+        }
+        return pq.size();
+    }
+};
