253045c15fc0644605af1b6febb769a4a026cee7
yashgautam12049 <97456693+yashgautam12049@users.noreply.github.com>
2024-10-21 15:59:17 +0530

    Time: 3 ms (94.09%), Space: 13 MB (13.18%) - LeetHub

--- 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.cpp
+++ 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.cpp
@@ -1,35 +1,51 @@
 class Solution {
 public:
-    bool parseBoolExpr(string expression) {
-        stack<char> st;
-
-        for (char currChar : expression) {
-            if (currChar == ',' || currChar == '(')
-                continue;  
-            if (currChar == 't' || currChar == 'f' || currChar == '!' ||
-                currChar == '&' || currChar == '|') {
-                st.push(currChar);
-            }
-            else if (currChar == ')') {
-                bool hasTrue = false, hasFalse = false;
-
-                while (st.top() != '!' && st.top() != '&' && st.top() != '|') {
-                    char topValue = st.top();
-                    st.pop();
-                    if (topValue == 't') hasTrue = true;
-                    if (topValue == 'f') hasFalse = true;
-                }
-                char op = st.top();
-                st.pop();
-                if (op == '!') {
-                    st.push(hasTrue ? 'f' : 't');
-                } else if (op == '&') {
-                    st.push(hasFalse ? 'f' : 't');
-                } else {
-                    st.push(hasTrue ? 't' : 'f');
-                }
+char func(char a,vector<char>temp){
+    if(a=='&'){
+        for(int i=0;i<temp.size();i++){
+            if(temp[i]=='f'){
+                return 'f';
             }
         }
-        return st.top() == 't';
+                return 't';
+    }
+    if(a=='|'){
+        for(int i=0;i<temp.size();i++){
+            if(temp[i]=='t'){
+                return 't';
+            }
+        }
+                return 'f';
+    }
+    if(a=='!'){
+        
+            if(temp[0]=='t'){
+                return 'f';
+            }else{
+                return 't';
+            }
+    }
+    return 'a';
+    
+}
+    bool parseBoolExpr(string s) {
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='|' || s[i]=='&' || s[i]=='!' ||s[i]=='(' ||s[i]=='f' || s[i]=='t'){
+                st.push(s[i]);
+            }
+            else if(s[i]==')'){
+                vector<char>temp;
+                while(st.top()!='('){
+                    temp.push_back(st.top());
+                    st.pop();
+                }
+                st.pop();
+                char op=st.top();
+                st.pop();
+                st.push(func(op,temp));
+            }
+        }
+        return st.top()=='t';
     }
 };
